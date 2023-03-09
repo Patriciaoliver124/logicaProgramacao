39e5cb1ec7760e7bad2318475f21bf3fa2fb33b3
Patriciaoliver124 <2022103030055@VHA-LAB3-29>
2023-03-09 14:21:50 -0400

    Criacao do projeto teste

---
+++ Projeto teste/Projeto teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola mundo!");
+            Console.ReadKey(); 
+            
+        }
+    }
+}
